baaee1daba9f9c7b61b5a86531198d824cf42cc2
baliram072x <baliramrathod0008@gmail.com>
2025-08-20 12:49:25 +0530

    update

---
+++ functions/problems/pattern 1.js
@@ -0,0 +1,45 @@
+// // given a value n(positive integer),
+// //print the following pattern on the screen 
+
+// // we have to print nxn grid
+
+
+// // let stars = "";
+
+// // stars += "*"
+// // stars += "*"
+// // stars += "*"
+// // stars += "*"
+// // stars += "*"
+
+// // console.log(stars)
+
+
+// function pattern(n){
+// for (let row = 1; row <= n; row++) {
+//     //task
+//     //the task is to contaatinate "* " n times
+//     let star = "";
+//     for (let col = 1; col <= n;  col++) {
+//         star += "*";
+
+//     }
+//     console.log(star)
+// }
+
+// }
+
+// pattern(6);
+// pattern(8);
+
+
+// // let x= "abc"
+// // let y= "def"
+// //let z= "xyz"
+
+// // let  r= x+y
+
+
+let x= 2;
+
+console.log(x+=3);
