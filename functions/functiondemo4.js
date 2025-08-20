b2046be83594fcf76d70b8261c98d3734d0dff0e
baliram072x <baliramrathod0008@gmail.com>
2025-08-20 10:51:33 +0530

    update

---
+++ functions/functiondemo4.js
@@ -0,0 +1,14 @@
+function add(x, y) {
+    let c = x + y;
+    return c;
+}
+
+let a = 10;
+let b = 20;
+let result = add(a , b);
+console.log(result)
+
+//here we are passing add(9,1) inside console.log
+// at first of all add(9,1) will parocessed
+
+console.log(add(9,1,10));
---
+++ functions/porblems/problem 1.js
@@ -0,0 +1,29 @@
+// given a number x, write afunction to dertermine 
+//weather the function is a prime number or not?
+// ----->
+
+/* how to cheak if a number is a prime or not?
+so what types of numbers are prime?
+eg x=3, x=5
+ beacuse prime are only divisible by 1 and the number itself
+ 
+ if there is atleat one more number apart from 1 and x 
+ that divides x completly then x in nonprime
+
+
+
+
+*/
+function isprime(x) {
+    if (x <= 1) return false; 
+   for( let i=2; i<=x-1; i+=1){
+    if(x%i ==0){
+        return false
+    }
+   }
+   return true
+}
+
+let a = 1;
+let result = isprime(a)
+console.log(result)
