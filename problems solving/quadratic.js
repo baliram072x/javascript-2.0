1fb8801f67736fa8803020be51e9ba006988f2e9
baliram072x <baliramrathod0008@gmail.com>
2025-08-25 22:03:02 +0530

    update

---
+++ problems solving/quadratic.js
@@ -0,0 +1,8 @@
+function solve(a, b, c) {
+    let sqrtvalue = Math.sqrt(b * b - 4 * a * c);
+    let root1 = (-b + sqrtvalue) / (2 * a);
+    let root2 = (-b - sqrtvalue)/(2*a);
+    console.log(root1, root2)
+}
+
+solve(2, 5, 3)
---
+++ problems solving/rootsofquadratic.js
@@ -0,0 +1,17 @@
+console.log("abc" == "abc");
+
+let x = "abc";
+let y = "abc";
+console.log(x == y);
+
+let z = String("abc");
+let a = new String("abc");
+console.log(z)
+
+console.log(a == z)
+console.log(a == x)
+console.log(z == x)
+
+console.log(a === z)
+console.log(a ===x)
+console.log(z === x)
